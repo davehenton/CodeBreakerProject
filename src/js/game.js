7bf9a321741e840f6bd614b7e2b40af8d717947f
Cristina Tarantino <m.cristina.tarantino@gmail.com>
2018-07-05 13:44:36 +0100

    implemented singleton game and redone tests accordingly

--- src/js/game.js
+++ src/js/game.js
@@ -1,147 +1,187 @@
-const MAX_NUM = 10000;
+window.game = (function() {
+  const MAX_NUM = 10000;
+  let answer;
+  let attempt;
+  let fragment;
 
-function $(selector, container) {
-  return (container || document).querySelector(selector);
-}
+  // Instance stores a reference to the Singleton
+  let instance;
 
-let answer = $('#answer');
-let attempt = $('#attempt');
-
-/**
- * guess The main method of the game
- */
-function guess() {
-  let input = $('#user-guess');
-
-  //Only set the answer and attempt hidden inputs when they aren't already set
-  if (answer.value === '' || attempt.value === '') {
-    setHiddenFields();
+  /**
+   * Singleton Init the game
+   */
+  function init() {
+    fragment = $('#game').innerHTML;
+    start();
   }
 
-  //validateInput if the length of the input is not 4
-  if (!validateInput(input.value)) {
-    return;
+  function start() {
+    answer = $('#answer');
+    attempt = $('#attempt');
+    $('#guess').addEventListener('click', guess);
+    $('#reload').addEventListener('click', reset);
   }
 
-  // increment the number of attempts
-  attempt.value++;
+  /**
+   * guess The main method of the game
+   */
+  function guess() {
+    let input = $('#user-guess');
 
-  if (getResults(input.value)) {     //if the user has guessed correctly
-    setMessage('You Win! :)');
-    showAnswer(true);
-    showReplay();
-  } else if (attempt.value >= 10) { //if the user has exceed the number of attempts
-    setMessage('You Lose! :(');
-    showAnswer(false);
-    showReplay();
-  } else {
-    setMessage('Incorrect, try again.'); //otherwise keep playing
+    //Only set the answer and attempt hidden inputs when they aren't already set
+    if (answer.value === '' || attempt.value === '') {
+      setHiddenFields();
   }
-}
 
-/**
- * getRandomInt randomly generated an integer between 0 and max number
- * @param max maximum number within which generate the random number
- * @returns {number} the random number
- */
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
+    //validateInput if the length of the input is not 4
+    if (!validateInput(input.value)) {
+      return;
+    }
 
-/**
- * setHiddenFields Set the answer variable equal to a randomly generated whole number between 0 and 9999
- */
-function setHiddenFields() {
-  // Set the hidden input attempt's value to zero
-  attempt.value = 0;
+    // increment the number of attempts
+    attempt.value++;
 
-  // Sets the answer variable equal to a randomly generated whole number between 0 and 9999
-  answer.value = getRandomInt(MAX_NUM).toString();
-
-  // Make sure the hidden input answer's value is exactly 4 characters long
-  while (answer.value.length < 4) {
-    answer.value = `0${answer.value}`;
-  }
-}
-
-/**
- * setMessage Set the #message label to whatever is provided to the parameter
- * @param message
- */
-function setMessage(message) {
-  document.getElementById('message').innerHTML = message;
-}
-
-/**
- * validateInput Validates the length of the parameter input
- * @param input The String that needs validating
- * @returns {boolean} If the parameter has a `length` of 4, return `true` —
- *                    otherwise return `false`
- */
-function validateInput(input) {
-  if (input.length !== 4) {
-    // use the `setMessage` function to set the `message` label to `"Guesses must be exactly 4 characters long."`,
-    setMessage('Guesses must be exactly 4 characters long.');
-    return false;
-  }
-  return true;
-}
-
-/**
- * getResults Add the results of the user's guess to our `results` div's `innerHTML`
- * @param input The value the user guessed
- * @returns {boolean} If all characters were guessed correctly, the function return `true`, otherwise `false`
- */
-function getResults(input) {
-  //create html to insert into #results
-  let html = `<div class="row"><span class="col-md-6">${input}</span><div class="col-md-6">`;
-
-  //for each character
-  for (let i = 0; i < input.length; i++) {
-    if (input.charAt(i) === answer.value.charAt(i)) {
-
-      //if the character is in the correct position in the `answer`
-      html += '<span class="glyphicon glyphicon-ok"></span>'; //add icon ok
-    } else if (answer.value.indexOf(input.charAt(i)) > -1) {
-
-      //if the character is in the `answer` but isn't in the right position
-      html += '<span class="glyphicon glyphicon-transfer"></span>'; //add icon transfer
+    if (getResults(input.value)) {     //if the user has guessed correctly
+      setMessage('You Win! :)');
+      showAnswer(true);
+      showReplay();
+    } else if (attempt.value >= 10) { //if the user has exceed the number of attempts
+      setMessage('You Lose! :(');
+      showAnswer(false);
+      showReplay();
     } else {
-
-      //if the number isn't in the `answer` at all
-      html += '<span class="glyphicon glyphicon-remove"></span>'; //add icon remove
+      setMessage('Incorrect, try again.'); //otherwise keep playing
     }
   }
 
-  //close html tags
-  html += '</div></div>';
-
-  //append the html to the #return element
-  document.getElementById('results').innerHTML += html;
-
-  return input === answer.value;
-}
-
-/**
- * showAnswer Display the Answer and updates the UI according to the parameter value
- * @param success true if the player has won, false otherwise
- */
-function showAnswer(success) {
-  let code = document.getElementById('code');
-
-  if (success) {
-    code.className += ' success';
-  } else {
-    code.className += ' failure';
+  /**
+   * getRandomInt randomly generated an integer between 0 and max number
+   * @param max maximum number within which generate the random number
+   * @returns {number} the random number
+   */
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
   }
 
-  code.innerHTML = answer.value;
-}
+  /**
+   * setHiddenFields Set the answer variable equal to a randomly generated whole number between 0 and 9999
+   */
+  function setHiddenFields() {
+    // Set the hidden input attempt's value to zero
+    attempt.value = 0;
 
-/**
- * showReplay Change the UI so the user can start over after they win or lose the game
- */
-function showReplay() {
-  document.getElementById('guessing-div').style.display = 'none';
-  document.getElementById('replay-div').style.display = 'block';
-}
+    // Sets the answer variable equal to a randomly generated whole number between 0 and 9999
+    answer.value = getRandomInt(MAX_NUM).toString();
+
+    // Make sure the hidden input answer's value is exactly 4 characters long
+    while (answer.value.length < 4) {
+      answer.value = `0${answer.value}`;
+    }
+  }
+
+  /**
+   * setMessage Set the #message label to whatever is provided to the parameter
+   * @param message
+   */
+  function setMessage(message) {
+    $('#message').innerHTML = message;
+  }
+
+  /**
+   * validateInput Validates the length of the parameter input
+   * @param input The String that needs validating
+   * @returns {boolean} If the parameter has a `length` of 4, return `true` —
+   *                    otherwise return `false`
+   */
+  function validateInput(input) {
+    if (input.length !== 4) {
+      // use the `setMessage` function to set the `message` label to `"Guesses must be exactly 4 characters long."`,
+      setMessage('Guesses must be exactly 4 characters long.');
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * getResults Add the results of the user's guess to our `results` div's `innerHTML`
+   * @param input The value the user guessed
+   * @returns {boolean} If all characters were guessed correctly, the function return `true`, otherwise `false`
+   */
+  function getResults(input) {
+    //create html to insert into #results
+    let html = `<div class="row"><span class="col-md-6">${input}</span><div class="col-md-6">`;
+
+    //for each character
+    for (let i = 0; i < input.length; i++) {
+      if (input.charAt(i) === answer.value.charAt(i)) {
+
+        //if the character is in the correct position in the `answer`
+        html += '<span class="glyphicon glyphicon-ok"></span>'; //add icon ok
+      } else if (answer.value.indexOf(input.charAt(i)) > -1) {
+
+        //if the character is in the `answer` but isn't in the right position
+        html += '<span class="glyphicon glyphicon-transfer"></span>'; //add icon transfer
+      } else {
+
+        //if the number isn't in the `answer` at all
+        html += '<span class="glyphicon glyphicon-remove"></span>'; //add icon remove
+      }
+    }
+
+    //close html tags
+    html += '</div></div>';
+
+    //append the html to the #return element
+    $('#results').innerHTML += html;
+
+    return input === answer.value;
+  }
+
+  /**
+   * showAnswer Display the Answer and updates the UI according to the parameter value
+   * @param success true if the player has won, false otherwise
+   */
+  function showAnswer(success) {
+    let code;
+    code = $('#code');
+
+    if (success) {
+      code.className += ' success';
+    } else {
+      code.className += ' failure';
+    }
+
+    code.innerHTML = answer.value;
+  }
+
+  /**
+   * showReplay Change the UI so the user can start over after they win or lose the game
+   */
+  function showReplay() {
+    $('#guessing-div').style.display = 'none';
+    $('#replay-div').style.display = 'block';
+  }
+
+  /**
+   * reload Reloads the page
+   */
+  function reset() {
+    attempt.value = '';
+    answer.value = '';
+    $('#game').innerHTML = fragment;
+    start();
+  }
+
+  return {
+    // Get the Singleton instance if one exists
+    // or create one if it doesn't
+    start: function () {
+
+      if ( !instance ) {
+        instance = init();
+      }
+
+      return instance;
+    }
+  };
+})();
---
+++ test/game.spec.js
@@ -0,0 +1,237 @@
+'use strict';
+
+//util function
+function getProperty(selector, property){
+  let elem = $(selector);
+  return window.getComputedStyle(elem,null).getPropertyValue(property);
+}
+
+describe('Calculator', function() {
+  // // inject the HTML fixture for the tests
+  beforeEach(function() {
+    // Why this line? See: https://github.com/billtrik/karma-fixture/issues/3
+    fixture.base = 'test';
+    fixture.load('game.fixture.html');
+
+    // init js lib
+    window.game.start();
+  });
+
+  // remove the html fixture from the DOM
+  afterEach(function() {
+    fixture.cleanup();
+  });
+
+  it('A', function() {
+    // $('#guess').click();
+    $('#answer').value.should.equal('');
+  });
+
+  describe('setHiddenFields()', function() {
+    it('should set the `value` of element `answer` to a random whole number between 0 and 9999. @randomNumber', function() {
+      assert(typeof $('#answer').value === "string", '`answer.value` is not of type string.');
+
+      let array = [];
+
+      for (let i = 0; i < 10; i++) {
+        $('#guess').click();
+        array.push($('#answer').value);
+        $('#reload').click();
+      }
+
+      array.sort();
+
+      assert(array.length > 0, '`setHiddenFields` didn\'t change the `value` of the `answer` hidden field. `setHiddenFields` should change the `answer.value` variable.');
+      let current = null;
+      let duplicates = 0;
+      for (let i = 0; i < array.length; i++) {
+        assert(array[i] >= 0 && array[i] <= 9999, '`answer.value` was ' + array[i] + ' which is not between 0 and 9999.');
+        assert(array[i].indexOf('.') === -1, '`answer.value` was ' + array[i] + ' which is not a whole number.');
+        if (array[i] !== current) {
+          current = array[i];
+        } else {
+          duplicates++;
+        }
+      }
+      assert(duplicates < 3, '`setHiddenFields` was run 10 times and `answer.value` was the same more than 3 times. `answer.value` does not appear to be random.');
+    });
+    it('should set the `value` of element `answer` to a number exactly 4 characters long. @answerLength', function () {
+      let array = [];
+
+      for (let i = 0; i < 10; i++) {
+        $('#guess').click();
+        array.push($('#answer').value);
+        $('#reload').click();
+      }
+
+      array.sort();
+
+      assert(array.length > 0, '`setHiddenFields` didn\'t change the `value` of the `answer` hidden field. `setHiddenFields` should change the `answer.value` variable.');
+      for (let i = 0; i < array.length; i++) {
+        assert(array[i].length === 4, 'The `value` of the element `answer` was ' + array[i] + ' which is not exactly 4 characters long.');
+      }
+    });
+    it('should set the `value` of element `attempt` to 0. @setAttempt', function () {
+      $('#guess').click();
+      assert($('#attempt').value === "0", 'The `value` of element `attempt` was "' + $('#attempt').value + '" which is not 0.');
+    });
+    it('should only run if the `value` of elements `attempt` or `answer` are empty. @preventClearing', function () {
+      $('#guess').click();
+      let expectedAnswer = $('#answer').value;
+      assert(expectedAnswer !== '', '`setHiddenFields` didn\'t change the `value` of the `answer` hidden field. `setHiddenFields` should change the `answer.value` variable.');
+      assert(expectedAnswer === $('#answer').value, 'the `value` of element `answer` was changed even though `answer` had already been provided a `value`. Only update the `value` of `answer` when answer is empty; `(\'\')`');
+    });
+  });
+  describe('setMessage()', function () {
+    it('should set element `message` to incorrect string value message. @setMessage', function () {
+      $('#guess').click();
+
+      assert($('#message').innerHTML === 'Guesses must be exactly 4 characters long.', 'test `setMessage` function.', '`message`s `innerHTML` did not match the provided value.');
+    });
+    it('should set element `message` to incorrect value message. @setMessage', function () {
+      $('#user-guess').value='0123';
+      $('#guess').click();
+
+      assert($('#message').innerHTML === 'Incorrect, try again.', 'test `setMessage` function.', '`message`s `innerHTML` did not match the incorrect value message.');
+    });
+    it('should set element `message` to winning message. @setMessage', function () {
+      $('#guess').click();
+      $('#user-guess').value=$('#answer').value;
+      $('#guess').click();
+
+      assert($('#message').innerHTML === 'You Win! :)', 'test `setMessage` function.', '`message`s `innerHTML` did not match the winning message.');
+    });
+    it('should set element `message` to loosing message. @setMessage', function () {
+      $('#user-guess').value='0000';
+
+      for(let i = 0; i < 10; i++) {
+        $('#guess').click();
+      }
+
+      assert($('#message').innerHTML === 'You Lose! :(', '`message`s `innerHTML` did not match the loosing message.');
+    });
+  });
+
+  describe('validateInput()', function () {
+    it('should accept one parameter and return `true` only when that parameter\'s `length` is 4. @validateInput', function () {
+      $('#user-guess').value='0000';
+      $('#guess').click();
+      assert($('#message').innerHTML === 'Incorrect, try again.', 'test `setMessage` function.', '`message`s `innerHTML` did not match the provided value.');
+
+      $('#user-guess').value='123';
+      $('#guess').click();
+      assert($('#message').innerHTML === 'Guesses must be exactly 4 characters long.', 'test `setMessage` function.', '`message`s `innerHTML` did not match the provided value.');
+    });
+
+    it('should run when `guess` runs @runValidation', function () {
+      let attempt = $('#attempt');
+      assert(attempt.value === "", 'The `value` of `attempt` increased when `validateInput` returned `false`, this should only iterate when `validateInput` returns `true`.');
+      $('#user-guess').value='0000';
+      $('#guess').click();
+      assert(attempt.value === "1", 'The `value` of `attempt` should increase by 1 when `validateInput` returns `true`.');
+    });
+  });
+
+  describe('getResults() @getResults', function () {
+    it('should accept one parameter and add to the `results` element\'s `innerHTML` based on how close the parameter is to the value of element `answer`. \n\tEach result should begin with `<div class="row"><span class="col-md-6">\' + input + \'</span><div class="col-md-6">` where `input` is the function\'s parameter. \n\tForeach character in `input`: add `<span class="glyphicon glyphicon-ok"></span>` \n\tif the character is in the element `answer`\s `value` and in the correct position, add `<span class="glyphicon glyphicon-transfer"></span>` \n\tif the character is in element `answer`\`s `value`, but is not in the correct position, and add `<span class="glyphicon glyphicon-remove"></span>` \n\tif the character is not in `answer`\'s `value` at all. @getResults', function () {
+      let guess = $('#guess');
+      let results = $('#results');
+      //Setup
+      guess.click();
+      $('#answer').value = '1234';
+
+      //first character correct, second wrong place, third and fouth wrong
+      $('#user-guess').value='1300';
+      guess.click();
+
+      //add tests for starting and stopping divs?
+      //add tests for each result?
+
+      let oks = (results.innerHTML.match(/glyphicon-ok/g) || []).length;
+      let transfers = (results.innerHTML.match(/glyphicon-transfer/g) || []).length;
+      let removes = (results.innerHTML.match(/glyphicon-remove/g) || []).length;
+
+      assert(oks > 0, 'correct characters are not being marked as correct.');
+      assert(oks < 2, 'some characters are being incorrectly marked as correct.');
+      assert(transfers > 0, 'characters that were present in the answer, but not in the right position are not getting marked correctly.');
+      assert(transfers < 2, 'some characters are being incorrectly marked as in the answer, but not in the right position.');
+      assert(removes > 1, 'characters not present in the answer are not being marked as wrong.');
+      assert(removes < 3, 'some characters are being incorrectly marked as not in the answer.');
+    });
+  });
+
+  describe('showAnswer()', function () {
+    it('should accept one parameter and set the `innerHTML` of element `code` to match element `answer`\'s value. If parameter is `true` element `code` should have ` success` added to it\'s `className`. (Note: the spaces before ` success`) @showAnswer', function () {
+      let code = $('#code');
+      let answer = $('#answer');
+      //test true
+      let guess = $('#guess');
+      guess.click();
+      $('#user-guess').value = answer.value;
+      guess.click();
+
+      assert(answer.value === code.innerHTML, '`code`\'s `innerHTML` did not match the `answer`\'s value when parameter was `true`.');
+      assert(code.className.indexOf(' success') !== -1, '`code`\'s `className` did not have ` success` in it when parameter was `true`.');
+    });
+    it('should accept one parameter and set the `innerHTML` of element `code` to match element `answer`\'s value. If parameter is `false` element `code` should have ` failure` added to it\'s `className`. (Note: the spaces before ` failure` are required) @showAnswer', function () {
+      let code = $('#code');
+      //test false
+      $('#user-guess').value='0000';
+
+      for(let i = 0; i < 10; i++) {
+        $('#guess').click();
+      }
+
+      assert($('#answer').value === code.innerHTML, '`code`\'s `innerHTML` did not match the `answer`\'s value when parameter was `false`.');
+      assert(code.className.indexOf(' failure') !== -1, '`code`\'s `className` did not have ` failure` in it when parameter was `false`.');
+    });
+  });
+
+  describe('showReplay()', function () {
+    it('should set element `guessing-div`\'s `style` to "display:none" element `replay-div`\'s `style` to "display:block" if win. @showReplay', function () {
+      let guess = $('#guess');
+      guess.click();
+      $('#user-guess').value = $('#answer').value;
+      guess.click();
+
+      assert(getProperty("#guessing-div", "display") === "none", '`guessing-div`\'s `style` was not set to "display:none".');
+      assert(getProperty("#replay-div", "display") === "block", '`replay-div`\'s `style` was not set to "display:block".');
+    });
+
+    it('should set element `guessing-div`\'s `style` to "display:none" element `replay-div`\'s `style` to "display:block" if lost. @showReplay', function () {
+
+      $('#user-guess').value='0000';
+
+      for(let i = 0; i < 10; i++) {
+        $('#guess').click();
+      }
+
+      assert(getProperty("#guessing-div", "display") === "none", '`guessing-div`\'s `style` was not set to "display:none".');
+      assert(getProperty("#replay-div", "display") === "block", '`replay-div`\'s `style` was not set to "display:block".');
+    });
+  });
+
+  describe('reset()', function () {
+    beforeEach(function() {
+      let guess = $('#guess');
+      guess.click();
+      $('#user-guess').value = $('#answer').value;
+      guess.click();
+
+      $('#reload').click();
+    });
+    it('should set the answer value to zero. @reset', function () {
+      assert($('#answer').value === '', '`answer.value` was not set to an empty string.');
+    });
+    it('should set the message to empty string. @reset', function () {
+      assert($('#attempt').value === '', '`attempt.value` was not set to to an empty string.');
+    });
+    it('should reset guesses and results. @reset', function () {
+      assert($('#results').children.length === 1, 'the results and guesses column html where not removed from the dom');
+    });
+    it('should set element `guessing-div`\'s `style` to "display:block" element `replay-div`\'s `style` to "display:none" if lost. @reset', function () {
+      assert(getProperty("#guessing-div", "display") === "block", '`guessing-div`\'s `style` was not set to "display:block".');
+      assert(getProperty("#replay-div", "display") === "none", '`replay-div`\'s `style` was not set to "display:none".');
+    });
+  });
+});
